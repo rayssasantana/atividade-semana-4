034581d16e679ca9397345dfaa8a922c5a7fc87c
rayssasantana <rayssasantana.br@gmail.com>
2024-07-06 15:15:20 -0300

    aula0507: alterado texto banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
     return (
         <section className="banner-bg" alt="Banner lilás">
             <h1 className="banner-titulo">Rayssa Santana</h1>
-            <p className="banner-texto">Fullstack Developer Portfólio</p>
+            <p className="banner-texto">Fullstack Developer Portfólio<br/><small>(Fictício)</small></p>
         </section>
     );
 }
